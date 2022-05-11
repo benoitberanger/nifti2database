cd7aeb802002dea12b18c76690daee00baa3a143
Benoît Béranger <benoit.beranger@icm-institute.org>
2022-05-11 20:04:03 +0200

    update the table and its initialization script

--- db_scripts/create_table__nifti_json.sql
+++ db_scripts/create_table__nifti_json.sql
@@ -1,7 +1,7 @@
 CREATE TABLE nifti2database_schema.nifti_json (
 	dict jsonb NOT NULL,
 	suid varchar(70) NOT NULL,
-	patient_name varchar(64) NOT NULL,
+	patient_id varchar(64) NOT NULL,
 	insertion_time timestamptz NOT NULL,
 	CONSTRAINT nifti_json_pk PRIMARY KEY (suid)
 );
@@ -11,7 +11,7 @@ GRANT ALL ON TABLE nifti2database_schema.nifti_json TO nifti2database_app;
 select * from nifti2database_schema.nifti_json;
 
 INSERT INTO nifti2database_schema.nifti_json
-(dict, seriesinstanceuid, insertion_time)
-VALUES('{}', 'abcd', now());
+(dict, suid, patient_id, insertion_time)
+VALUES('{}', '0.0.0.0', '2022_01_01_DEV2_XXX', now());
 
 select * from nifti2database_schema.nifti_json;
